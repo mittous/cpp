7a664e2827124150ecd5f12ba387abf72d4635aa
mittous <issmaimit123@gmail.com>
2023-03-27 13:32:55 +0000

    finish ex00

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -5,9 +5,11 @@
 
 int main()
 {
-    
-
+    Zombie *walker = newZombie("ismail");
     randomChump("mittous");
 
+    walker->announce();
+
+    delete walker ;
     return 0;
 }
--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -4,6 +4,5 @@
 void randomChump(std::string name)
 {
     Zombie walker(name);
-    
     walker.announce();
 }
