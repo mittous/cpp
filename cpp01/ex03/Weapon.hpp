55f8ae2b83da7a266599686f68aeb0c04a809409
mittous <issmaimit123@gmail.com>
2023-03-29 10:58:50 +0000

    add necesserie files for cpp01/ex03

---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,29 @@
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+
+# include <iostream>
+# include <string>
+
+class Weapon
+{
+private:
+    std::string type;
+public:
+    Weapon();
+    ~Weapon();
+};
+
+Weapon::Weapon()
+{
+}
+
+Weapon::~Weapon()
+{
+}
+
+
+
+
+#endif
