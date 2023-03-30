b2552f7597a49fee0adc585c34cbafa8f1b9ab35
mittous <imittous@student.1337.ma>
2023-03-30 18:31:25 +0000

    first attempt  cpp01/ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,1 +1,35 @@
 #include <fstream>
+#include <string>
+#include <iostream>
+
+int main (int ac, char **av)
+{
+	if (ac == 4)
+	{
+		std::ifstream myfile;
+		std::string fileContent;
+		std::string new_string;
+		int indx = 0;
+		myfile.open(av[1]);
+		getline(myfile, fileContent, '\0');
+
+		while (indx < fileContent.length())
+		{
+			size_t pos = fileContent.find(av[2]);
+			if (pos)
+			{
+				new_string += fileContent.substr(indx, pos);
+				indx = pos+strlen(av[1]);
+			}
+			// if (pos!=std::string::npos)
+
+
+			indx++;
+		}
+		
+		std::ofstream dupfile("new_file.txt");
+		dupfile << new_string;
+		dupfile.close();
+	}
+}
+		// std::cout << new_string;
