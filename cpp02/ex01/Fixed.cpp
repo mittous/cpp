4c0ced1ad204268c8c64e8721d052d4c0097ac52
mittous <issmaimit123@gmail.com>
2023-04-09 10:13:27 +0000

    cpp01/ex01 is finish Sun 09 Apr 2023 10:16:17 AM +00

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,59 +1,65 @@
 #include "Fixed.hpp"
+#include <cmath>
 
-// Constructors
-Fixed::Fixed()
+Fixed::Fixed() : value_(0)
 {
-	std::cout << "\e[0;33mDefault Constructor called of Fixed\e[0m" << std::endl;
-}
-
-Fixed::Fixed(const Fixed &copy)
-{
-	std::cout << "\e[0;33mCopy Constructor called of Fixed\e[0m" << std::endl;
-	this->value = copy.value;
+    std::cout << "\e[0;33mDefault constructor called\e[0m" << std::endl;
 }
 
 Fixed::Fixed(const int _value)
 {
-	this->value = value * 256;
+    std::cout << "\e[0;33mInt constructor called\e[0m" << std::endl;
+	this->value_ = _value * 256;
 }
 
 Fixed::Fixed(const float _value)
 {
-	this->value = value * 256;
+    std::cout << "\e[0;33mFloat constructor called\e[0m" << std::endl;
+	value_ = roundf(_value * 256);
 }
 
-// Destructor
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "\e[0;33mCopy constructor called\e[0m" << std::endl;
+	value_ = other.value_;
+}
+
 Fixed::~Fixed()
 {
-	std::cout << "\e[0;31mDestructor called of Fixed\e[0m" << std::endl;
+    std::cout << "\e[0;31mDestructor called\e[0m" << std::endl;
 }
 
-
-// Operators
-Fixed & Fixed::operator=(const Fixed &assign)
+Fixed& Fixed::operator=(const Fixed& other)
 {
-	if (this != &assign)
-		this->value = assign.value;
-	return *this;
+    std::cout << "\e[0;33mCopy assignment operator called\e[0m" << std::endl;
+    if (this != &other) 
+        value_ = other.value_;
+    return *this;
 }
 
-
-
-float Fixed::toFloat( void ) const
+int Fixed::getRawBits(void) const
 {
-	return ((float)this->value * 256);
+    std::cout << "\e[0;32mgetRawBits member function called\e[0m" << std::endl;
+    return value_;
 }
 
-
-
-
-int Fixed::toInt( void ) const
+void Fixed::setRawBits(int const raw) 
 {
-	return (this->value * 256);
+    std::cout << "\e[0;32msetRawBits member function called\e[0m" << std::endl;
+    value_ = raw;
 }
 
-
-std::ostream& operator<<(std::ostream &os, Fixed& obj)
+float Fixed::toFloat(void) const
 {
-	return os << obj.getRawBits();
+    return (float)value_ / 256;
+}
+
+int Fixed::toInt(void) const
+{
+    return value_ / 256;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+    os << fixed.toFloat();
+    return os;
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -1,35 +1,39 @@
-#ifndef FIXED_HPP
+# ifndef FIXED_HPP
 # define FIXED_HPP
 
 # include <iostream>
-# include <string>
+# include <cmath>
 
-class Fixed
+
+class Fixed 
 {
 private:
-	int value;
-	static const int fractionalBits = 8;
+
+    int value_;
+    static const int fractionalBits_ = 8;
+
 public:
-	// Canonical_form_requirment
-	Fixed();
-	Fixed(const Fixed &copy);
-	~Fixed();
-	Fixed & operator=(const Fixed &assign);
 
-	void setRawBits( int const raw );
-	int getRawBits(void) const;
-
-
-	// constractor overload
-	Fixed::Fixed(const int _value);
-	Fixed::Fixed(const float _value);
+	// canon
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed& other);
+    Fixed&	operator=(const Fixed& other);
+    
+	// set/get
+	int		getRawBits(void) const;
+    void	setRawBits(int const raw);
 
 
-	//
-	float toFloat( void ) const;
-	int toInt( void ) const;
+	// overload constr
+    Fixed(const int _value);
+    Fixed(const float _value);
+	
+	//requir
+    float	toFloat(void) const;
+    int		toInt(void) const;
 };
 
-std::ostream& operator<<(std::ostream &os, Fixed& obj);
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
 
-#endif
+# endif
