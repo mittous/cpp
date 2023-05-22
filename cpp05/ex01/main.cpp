195340c84e9623740947879ebc1b8c5e0f145a82
mittous <imittous@student.1337.ma>
2023-05-22 22:25:52 +0100

    starting cpp05/ex02

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -1,26 +1,43 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-int main(void)
-{
+// int main(void)
+// {
  
+//     try
+//     {
+//         Bureaucrat mitt("mittous", 0);
+//         Form form("form", 1, 1);
+//         try
+//         {
+//             form.beSigned(mitt);
+//         }
+//         catch(const std::exception& e)
+//         {
+//         }
+        
+//         mitt.signForm(form);
+//     }
+//     catch(std::exception & e)
+//     {
+//         std::cerr << e.what();
+//     }
+//     return (0);
+// }
+
+int main () try
+{
+    Bureaucrat mitt("mittous" , 0);
+    Form form("form1", 1, 1);
     try
     {
-        Bureaucrat mitt("mittous", 0);
-        Form form("form", 1, 1);
-        try
-        {
-            form.beSigned(mitt);
-        }
-        catch(const std::exception& e)
-        {
-        }
-        
-        mitt.signForm(form);
+        form.beSigned(mitt);
     }
-    catch(std::exception & e)
+    catch(std::exception &e)
     {
-        std::cerr << e.what();
     }
-    return (0);
+}
+catch(const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
 }
